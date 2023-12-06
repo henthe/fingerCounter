e7b12f07177fa3fab97c3707da8936e9804bdcd3
Dominik <dominik.zoric@adesso.de>
2023-12-06 15:26:59 +0100

    new changes related to read out pictures to csv

--- app.py
+++ app.py
@@ -227,7 +227,6 @@ def calc_landmark_list(image, landmarks):
 
     return landmark_point
 
-
 def pre_process_landmark(landmark_list):
     temp_landmark_list = copy.deepcopy(landmark_list)
 
@@ -244,6 +243,10 @@ def pre_process_landmark(landmark_list):
     temp_landmark_list = list(
         itertools.chain.from_iterable(temp_landmark_list))
 
+    # Check if temp_landmark_list is empty before normalization
+    if not temp_landmark_list:
+        return []
+
     # Normalization
     max_value = max(list(map(abs, temp_landmark_list)))
 
@@ -253,6 +256,32 @@ def pre_process_landmark(landmark_list):
     temp_landmark_list = list(map(normalize_, temp_landmark_list))
 
     return temp_landmark_list
+
+# def pre_process_landmark(landmark_list):
+#     temp_landmark_list = copy.deepcopy(landmark_list)
+#
+#     # Convert to relative coordinates
+#     base_x, base_y = 0, 0
+#     for index, landmark_point in enumerate(temp_landmark_list):
+#         if index == 0:
+#             base_x, base_y = landmark_point[0], landmark_point[1]
+#
+#         temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+#         temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+#
+#     # Convert to a one-dimensional list
+#     temp_landmark_list = list(
+#         itertools.chain.from_iterable(temp_landmark_list))
+#
+#     # Normalization
+#     max_value = max(list(map(abs, temp_landmark_list)))
+#
+#     def normalize_(n):
+#         return n / max_value
+#
+#     temp_landmark_list = list(map(normalize_, temp_landmark_list))
+#
+#     return temp_landmark_list
 
 
 def pre_process_point_history(image, point_history):
---
+++ generate_landmarks.py
@@ -0,0 +1,86 @@
+# generate_landmarks.py
+
+import os
+import cv2 as cv
+import mediapipe as mp
+from app import pre_process_landmark, logging_csv, KeyPointClassifier
+
+
+def predict_landmarks(image_path, hands_model, keypoint_classifier_model):
+    image = cv.imread(image_path)
+    if image is None:
+        print(f"Error: Unable to read the image at {image_path}")
+        return
+
+    image = cv.flip(image, 1)  # Mirror display
+
+    # Detection implementation
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+    image.flags.writeable = False
+    results = hands_model.process(image)
+    image.flags.writeable = True
+
+    # Process the results to get landmarks
+    landmark_list = []
+    if results.multi_hand_landmarks is not None:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Landmark calculation
+            landmark_list = calc_landmark_list(image, hand_landmarks)
+
+    # Convert landmarks to the format expected by the keypoint_classifier
+    pre_processed_landmark_list = pre_process_landmark(landmark_list)
+
+    #Check if the landmark list is not empty before logging
+    if pre_processed_landmark_list:
+        # Write the predicted landmarks to the keypoint.csv file
+        logging_csv(6, 1, pre_processed_landmark_list, [])
+
+    return pre_processed_landmark_list
+
+
+
+def calc_landmark_list(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_point = []
+
+    # Keypoint
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point.append([landmark_x, landmark_y])
+
+    return landmark_point
+
+
+def main():
+    # Load the trained models
+    hands_model = mp.solutions.hands.Hands(
+        static_image_mode=False,
+        max_num_hands=1,
+        min_detection_confidence=0.2,
+        min_tracking_confidence=0.5,
+    )
+    keypoint_classifier_model = KeyPointClassifier()
+
+    # Directory containing images for which you want to predict landmarks
+    image_dir = "C:\\Git1\\hand-gesture-recognition-mediapipe\\DatasetForFingerCounter\\Sub\\ImagesHenrik\\spock_Henrik"
+
+    for filename in os.listdir(image_dir):
+        if filename.endswith(".jpg"):
+            image_path = os.path.join(image_dir, filename)
+            print(f"Processing image: {image_path}")
+
+            # Predict landmarks for the current image
+            predicted_landmarks = predict_landmarks(
+                image_path, hands_model, keypoint_classifier_model
+            )
+
+            # Write the predicted landmarks to the keypoint.csv file
+            # logging_csv(0, 1, predicted_landmarks, [])
+
+
+if __name__ == '__main__':
+    main()
